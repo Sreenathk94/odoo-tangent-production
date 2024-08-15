202b07cd8b06bc056a437977701898cc52b18c60
fouvty <fouvtylive@gmail.com>
2024-08-15 15:29:15 +0530

    [add] - Initial commit for tg_base and hide_menu_user

---
+++ tg_base/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Tangent Base',
+    'version': '17.0.1.0.0',
+    'Summary': 'Base module for tangent system',
+    'author': 'fouvty',
+    'description': 'Alert Messages for the',
+    'website': '',
+    'category':'Human Resources',
+    'depends': ['base','hr_timesheet'],
+    'data': [
+        'data/res_company_views.xml',
+        'data/res_users_views.xml',
+    ],
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
