f83deccb91dcaa3ce1a10310c8d0e4ad5e0e4dc1
fouvty <fouvtylive@gmail.com>
2024-10-28 18:59:17 +0530

    [ADD & UPDT]- Added module bi_employee_timesheet_report and updates on hr_leave_sumbit_report

--- hr_leave_submit_report/models/hr_leave.py
+++ hr_leave_submit_report/models/hr_leave.py
@@ -1,36 +1,49 @@
-from odoo import models, api, fields
-from odoo.exceptions import ValidationError
+from odoo import api, models, fields
+from datetime import datetime, time
+
+from odoo.exceptions import ValidationError, AccessDenied
 
 
 class HrLeave(models.Model):
-    """Inherited model for hr.leave with additional methods"""
     _inherit = 'hr.leave'
 
-    @api.constrains('holiday_status_id', 'request_date_from', 'request_date_to',
-                    'supported_attachment_ids')
-    def check_leave_status(self):
-        """
-        Checks if the leave status is valid.
-        Valid leave statuses are 'sick'.
-        """
-        if self.holiday_status_id.id == self.env.ref(
-                'hr_holidays.holiday_status_sl').id:
-            if self.number_of_days_display and self.number_of_days_display >= 2 and not self.supported_attachment_ids:
-                raise ValidationError(
-                    "Employees must submit an attachment or a medical report for any sick leave lasting two or more consecutive days.")
+    def create(self, vals_list):
+        """Super the create function in hr.leave to calculate duration days."""
 
-            if self.number_of_days_display and self.number_of_days_display == 1 and not self.supported_attachment_ids:
-                request_date_from = self.request_date_from
-                date_obj = fields.Date.from_string(self.request_date_from)
+        res = super(HrLeave, self).create(vals_list)
+        if res.holiday_status_id.id == res.env.ref(
+                'hr_holidays.holiday_status_sl').id:
+            if res.number_of_days_display and res.number_of_days_display >= 2 and not res.supported_attachment_ids:
+                raise ValidationError(
+                    "Employee must submit the attachment or medical report for sick leave of 2 or more consecutive days.")
+            if res.number_of_days_display and res.number_of_days_display == 1 and not res.supported_attachment_ids:
+                request_date_from = res.request_date_from
+                print("request_date_from", request_date_from)
+                date_obj = fields.Date.from_string(res.request_date_from)
                 # Check if the day is Monday (0) or Friday (4)
+                print("Check", date_obj)
                 if date_obj.weekday() in (0, 4):
                     day = "Monday" if date_obj.weekday() == 0 else "Friday"
                     raise ValidationError(
                         f"Employee must submit an attachment or medical report if the leave starts on {day}"
                     )
+        return res
 
-    @api.depends('leave_type_support_document', 'attachment_ids')
-    def _compute_supported_attachment_ids(self):
-        for holiday in self:
-            holiday.supported_attachment_ids = holiday.attachment_ids
-            holiday.supported_attachment_ids_count = len(holiday.attachment_ids.ids)
+    @api.constrains('supported_attachment_ids')
+    def _onchange_supported_attachment_ids(self):
+        """This onchange method is used set validation if attatchment desont
+        exist"""
+        if not self.supported_attachment_ids:
+            if self.holiday_status_id.id == self.env.ref(
+                    'hr_holidays.holiday_status_sl').id:
+                if self.number_of_days_display and self.number_of_days_display >= 2 and not self.supported_attachment_ids:
+                    raise ValidationError(
+                        "Employee must submit the attachment or medical report for sick leave of 2 or more consecutive days.")
+                if self.number_of_days_display and self.number_of_days_display == 1 and not self.supported_attachment_ids:
+                    date_obj = fields.Date.from_string(self.request_date_from)
+                    # Check if the day is Monday (0) or Friday (4)
+                    if date_obj.weekday() in (0, 4):
+                        day = "Monday" if date_obj.weekday() == 0 else "Friday"
+                        raise ValidationError(
+                            f"Employee must submit an attachment or medical report if the leave starts on {day}"
+                        )
