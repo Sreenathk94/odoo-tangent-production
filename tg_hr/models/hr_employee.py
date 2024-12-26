3f558f6269c45ac40cae8b9f97c33343bf64754f
fouvty <fouvtylive@gmail.com>
2024-12-26 16:21:42 +0530

    [updt]-Employee view note book hide for normal users

--- tg_hr/models/hr_employee.py
+++ tg_hr/models/hr_employee.py
@@ -1,17 +1,25 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class HREmployee(models.Model):
-	_inherit = "hr.employee"
+    _inherit = "hr.employee"
 
-	passport_expire = fields.Date(string="Passport Expire Date",copy=False)
-	permit_expire = fields.Date(string="Labour Card Expire",copy=False)
-	emergency_mobile = fields.Char(string="Emergency Mobile")
+    passport_expire = fields.Date(string="Passport Expire Date", copy=False)
+    permit_expire = fields.Date(string="Labour Card Expire", copy=False)
+    emergency_mobile = fields.Char(string="Emergency Mobile")
+    is_hr_access = fields.Boolean(
+        string="Has HR Access",
+        compute="_compute_is_hr_access", readonly=True)
+
+    @api.depends('user_id')
+    def _compute_is_hr_access(self):
+        for record in self:
+            record.is_hr_access = self.env.user.has_group('tg_groups.group_employee_hr_details_access')
 
 
 class HREmployeePublic(models.Model):
-	_inherit = "hr.employee.public"
+    _inherit = "hr.employee.public"
 
-	passport_expire = fields.Date(string="Passport Expire Date",copy=False)
-	permit_expire = fields.Date(string="Labour Card Expire",copy=False)
-	emergency_mobile = fields.Char(string="Emergency Mobile")
+    passport_expire = fields.Date(string="Passport Expire Date", copy=False)
+    permit_expire = fields.Date(string="Labour Card Expire", copy=False)
+    emergency_mobile = fields.Char(string="Emergency Mobile")
