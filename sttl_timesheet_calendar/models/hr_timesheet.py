cda41ae31e04362949d07bfcff360b1571bef4e4
Fouvty Techno Solution <169172831+fouvty-live@users.noreply.github.com>
2025-01-01 21:51:00 +0530

    Update hr_timesheet.py

--- sttl_timesheet_calendar/models/hr_timesheet.py
+++ sttl_timesheet_calendar/models/hr_timesheet.py
@@ -41,7 +41,6 @@ class AccountAnalyticLine(models.Model):
         related='project_id.allowed_stage_ids')
     status_id = fields.Many2one(
         "hr.timesheet.status",'Status',order='sequence asc',
-        domain="[('id', 'in', allowed_stage_ids)]"
     )
     project_task_id = fields.Many2one(
         "project.task",'Task',order='sequence asc',
