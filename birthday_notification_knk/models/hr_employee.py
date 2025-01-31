2a1ea42ee282ed61560d31a1e611e2f36f4e0176
Fouvty Techno Solution <169172831+fouvty-live@users.noreply.github.com>
2025-01-31 14:39:54 +0400

    Update hr_employee.py

--- birthday_notification_knk/models/hr_employee.py
+++ birthday_notification_knk/models/hr_employee.py
@@ -4,25 +4,19 @@
 
 from odoo import api, fields, models
 import logging
-
+_logger = logging.getLogger(__name__)
 
 class HrEmployee(models.Model):
     _inherit = "hr.employee"
 
     @api.model
     def send_birthday_notification(self):
-        _logger = logging.getLogger(__name__)  # Corrected logger
         today = fields.Date.context_today(self)
         today_month_day = (today.month, today.day)
-        employees = self.env['hr.employee'].search([('birthday', '!=', False), ('work_email', '!=', False)])
-        for employee in employees:
+        for employee in self.env['hr.employee'].search([('birthday', '!=', False),('work_email', '!=', False)]):
             if employee.birthday:
                 employee_month_day = (employee.birthday.month, employee.birthday.day)
-                _logger.info('Checking birthday for: %s', employee.name)  # Corrected logging
                 if employee.company_id.send_employee_birthday_notification and today_month_day == employee_month_day:
-                    _logger.info('Sending birthday notification to: %s', employee.name)
+                    _logger.info('Birthday email sent to: %s', employee.work_email)
                     template_id = self.env.ref('birthday_notification_knk.employee_birthday_notification_template')
-                    if template_id:
-                        template_id.send_mail(employee.id, force_send=True)
-                    else:
-                        _logger.warning('Email template not found!')
+                    template_id.send_mail(employee.id, force_send=True)
