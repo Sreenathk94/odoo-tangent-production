a2e000face52817f70cbd307df06fef5b21294ff
Fouvty Techno Solution <169172831+fouvty-live@users.noreply.github.com>
2025-01-31 13:57:26 +0400

    Update hr_employee.py

--- birthday_notification_knk/models/hr_employee.py
+++ birthday_notification_knk/models/hr_employee.py
@@ -4,7 +4,6 @@
 
 from odoo import api, fields, models
 import logging
-logger = logging.getLogger(__name_)
 
 
 class HrEmployee(models.Model):
@@ -17,8 +16,10 @@ class HrEmployee(models.Model):
         for employee in self.env['hr.employee'].search([('birthday', '!=', False),('work_email', '!=', False)]):
             if employee.birthday:
                 employee_month_day = (employee.birthday.month, employee.birthday.day)
+                logger = logging.getLogger(__name_)
                 _logger.info('employee11111: %s', employee)
                 if employee.company_id.send_employee_birthday_notification and today_month_day == employee_month_day:
+                    logger = logging.getLogger(__name_)
                     _logger.info('employee222222: %s', employee)
                     template_id = self.env.ref('birthday_notification_knk.employee_birthday_notification_template')
                     template_id.send_mail(employee.id, force_send=True)
