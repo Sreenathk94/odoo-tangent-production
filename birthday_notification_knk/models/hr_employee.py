a724a9b232e8b8424613a78af1d3e54bdf500252
Fouvty Techno Solution <169172831+fouvty-live@users.noreply.github.com>
2025-01-31 14:21:00 +0400

    Update hr_employee.py

--- birthday_notification_knk/models/hr_employee.py
+++ birthday_notification_knk/models/hr_employee.py
@@ -11,15 +11,18 @@ class HrEmployee(models.Model):
 
     @api.model
     def send_birthday_notification(self):
+        _logger = logging.getLogger(__name__)  # Corrected logger
         today = fields.Date.context_today(self)
         today_month_day = (today.month, today.day)
-        for employee in self.env['hr.employee'].search([('birthday', '!=', False),('work_email', '!=', False)]):
+        employees = self.env['hr.employee'].search([('birthday', '!=', False), ('work_email', '!=', False)])
+        for employee in employees:
             if employee.birthday:
                 employee_month_day = (employee.birthday.month, employee.birthday.day)
-                logger = logging.getLogger(__name_)
-                _logger.info('employee11111: %s', employee)
+                _logger.info('Checking birthday for: %s', employee.name)  # Corrected logging
                 if employee.company_id.send_employee_birthday_notification and today_month_day == employee_month_day:
-                    logger = logging.getLogger(__name_)
-                    _logger.info('employee222222: %s', employee)
+                    _logger.info('Sending birthday notification to: %s', employee.name)
                     template_id = self.env.ref('birthday_notification_knk.employee_birthday_notification_template')
-                    template_id.send_mail(employee.id, force_send=True)
+                    if template_id:
+                        template_id.send_mail(employee.id, force_send=True)
+                    else:
+                        _logger.warning('Email template not found!')
