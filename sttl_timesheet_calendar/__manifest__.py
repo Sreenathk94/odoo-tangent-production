76581f048285e7237c2c078de14105207b67b95d
Midhlaj Nechikkandan <midh.exe@gmail.com>
2025-03-29 20:23:56 +0530

    fixes

--- sttl_timesheet_calendar/__manifest__.py
+++ sttl_timesheet_calendar/__manifest__.py
@@ -19,12 +19,12 @@
         'security/security.xml',
         'security/ir.model.access.csv',
         'views/hr_timesheet_view.xml',
-        # 'views/hr_timesheet_submit_view.xml',
-        # 'views/project_project_view.xml',
-        # 'views/hr_timesheet_submit_report_view.xml',
-        # # 'views/project_profit_report_view.xml',
-        # 'views/hr_individual_timesheet_report.xml',
-        # 'views/hr_employee_view.xml',
+        'views/hr_timesheet_submit_view.xml',
+        'views/project_project_view.xml',
+        'views/hr_timesheet_submit_report_view.xml',
+        # 'views/project_profit_report_view.xml',
+        'views/hr_individual_timesheet_report.xml',
+        'views/hr_employee_view.xml',
     ],
     'assets': {
         'web.assets_backend': [
